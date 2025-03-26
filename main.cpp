c5f4fa67efe778b7d047cd7cdbdf31ac0192ac3e
WesleyP2006 <wesleypilamung@gmail.com>
2025-03-26 17:52:44 -0400

    Added txt files

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    //The words in the keys.txt are shift words
+    //Bob decripts the message by shifting 
+    //Need to determine the shift word length, not the word itself
+
+
+    return 0;
+}
